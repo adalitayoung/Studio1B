1c51db0a1fd8e33f48750e103187dda1795930cc
Gabrielle Walker <13196732@student.uts.edu.au>
2020-04-17 14:04:40 +1000

    In progress: override button works, separated createRecord into separate function

--- Studio1B/CreateNewDiscount.swift
+++ Studio1B/CreateNewDiscount.swift
@@ -15,79 +15,11 @@ class CreateNewDiscount: StaffMenu {
     @IBOutlet weak var DiscountDescription_TF: UITextField!
     @IBOutlet weak var MissingDetailsMessage: UILabel!
     
-//    func handleOverride(DiscountName: String, DiscountValue: Float, _ DiscountDescription: String) -> (_ alertAction:UIAlertAction) -> () {
-//        self.createRecord(DiscountName:DiscountName,DiscountValue:DiscountValue,DiscountDescription:DiscountDescription)
-//    }
+    lazy var DiscountName = DiscountName_TF.text!
+    lazy var DiscountValue = Double(DiscountValue_TF.text!)
+    lazy var DiscountDescription = DiscountDescription_TF.text!
     
-    @IBAction func submitNewDiscount_BTN(_ sender: Any) {
-        let DiscountName: String = DiscountName_TF.text!
-        // let DiscountValue: Double = DiscountValue_TF.text!
-        var DiscountValue = (DiscountValue_TF.text as! NSString).floatValue
-        let DiscountDescription: String = DiscountDescription_TF.text!
-        
-        let alert = UIAlertController(title: "Already Exists",
-                                            message: "This discount already exists, do you want to override it?",
-                                            preferredStyle: .alert)
-               // Add action buttons to it and attach handler functions if you want to
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-//        alert.addAction(UIAlertAction(title: "Override", style: .destructive, handler: {action in self.handleOverride(DiscountName: String, DiscountValue: <#Float#>, <#String#>)
-//
-//
-//    }))
-//        alert.addAction(UIAlertAction(title: "Override", style: .destructive, handler: {action in
-//
-//            let docRef = db.collection("Rewards").document(DiscountName)
-//                docRef.setData([
-//                    "Deduction": DiscountValue,
-//                    "Description": DiscountDescription
-//                ])
-//                { err in
-//                    if let err = err {
-//                        print("Error adding document: \(err)")
-//                    } else {
-//                        print("Document added")
-//                    }
-//                }
-//        }))
-
-        if !(DiscountName.isEmpty) && (DiscountValue != nil){
-            // Check if the discount already exists
-            let docRef = db.collection("Rewards").document(DiscountName)
-
-            docRef.getDocument{(document, error) in
-                if let document = document, document.exists {
-                    // Show error message, choose new name or overwrite existing
-                    print("Document Exists")
-
-                // Show the alert by presenting it
-                self.present(alert, animated: true)
-
-
-                }
-                else {
-                  //  self.createRecord(DiscountName: <#String#>, DiscountValue: <#Double#>, DiscountDescription: <#String#>)
-                    self.createRecord(DiscountName:DiscountName, DiscountValue:DiscountValue, DiscountDescription:DiscountDescription)
-//                    docRef.setData([
-//                        "Deduction": DiscountValue,
-//                        "Description": DiscountDescription
-//                    ])
-//                    { err in
-//                        if let err = err {
-//                            print("Error adding document: \(err)")
-//                        } else {
-//                            print("Document added")
-//                        }
-//                    }
-                }
-            }
-        }
-        else {
-            MissingDetailsMessage.isHidden = false
-        }
-    }
-//
-    func createRecord(DiscountName: String, DiscountValue: Float, DiscountDescription: String) {
-
+    func createRecord(DiscountName: String, DiscountValue: Double, DiscountDescription: String) {
         let docRef = db.collection("Rewards").document(DiscountName)
 
         docRef.setData([
@@ -103,6 +35,76 @@ class CreateNewDiscount: StaffMenu {
         }
     }
     
+//    func handleOverride(DiscountName: String, DiscountValue: Float, _ DiscountDescription: String) -> (_ alertAction:UIAlertAction) -> () {
+//        self.createRecord(DiscountName:DiscountName,DiscountValue:DiscountValue,DiscountDescription:DiscountDescription)
+//    }
+    
+    @IBAction func submitNewDiscount_BTN(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Already Exists",
+                                            message: "This discount already exists, do you want to override it?",
+                                            preferredStyle: .alert)
+               // Add action buttons to it and attach handler functions if you want to
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Override", style: .destructive, handler: { action in
+            
+        self.createRecord(DiscountName: self.DiscountName, DiscountValue: self.DiscountValue!, DiscountDescription: self.DiscountDescription)
+
+//            let docRef = self.db.collection("Rewards").document(self.DiscountName)
+//
+//            docRef.setData([
+//                "Deduction": self.DiscountValue,
+//                "Description": self.DiscountDescription
+//            ])
+//            { err in
+//                if let err = err {
+//                    print("Error adding document: \(err)")
+//                } else {
+//                    print("Document added")
+//                }
+//            }
+        }))
+
+        if !(DiscountName.isEmpty) && (DiscountValue != nil){
+            // Check if the discount already exists
+            let docRef = db.collection("Rewards").document(DiscountName)
+
+            docRef.getDocument{(document, error) in
+                
+                if let document = document, document.exists {
+                    // Show error message, choose new name or overwrite existing
+                    print("Document Exists")
+
+                // Show the alert by presenting it
+                self.present(alert, animated: true)
+                    
+                }
+                else {
+                    
+                    self.createRecord(DiscountName: self.DiscountName, DiscountValue: self.DiscountValue!, DiscountDescription: self.DiscountDescription)
+                        
+//                    let docRef = self.db.collection("Rewards").document(DiscountName)
+//
+//                    docRef.setData([
+//                        "Deduction": DiscountValue,
+//                        "Description": DiscountDescription
+//                    ])
+//                { err in
+//                    if let err = err {
+//                        print("Error adding document: \(err)")
+//                    } else {
+//                        print("Document added")
+//                    }
+//                }
+                    }
+                }
+            }
+        else {
+            MissingDetailsMessage.isHidden = false
+        }
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
        
