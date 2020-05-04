fe15f4f39512b69b4ffcad300c50c26f9e9058d5
davidbolis <davidmaged1@hotmail.com>
2020-05-04 16:19:57 +1000

    Pod File Set Up YTB

---
+++ Studio1BTests/Studio1BTests.swift
@@ -0,0 +1,34 @@
+//
+//  Studio1BTests.swift
+//  Studio1BTests
+//
+//  Created by David Bolis on 4/5/20.
+//  Copyright © 2020 davidBolis. All rights reserved.
+//
+
+import XCTest
+
+@testable import Studio1B
+class Studio1BTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
