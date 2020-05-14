224c94d1b645f130ad1227098f54432c3b8d5c6c
Adalita Young <adalitayoung@Adalitas-MacBook-Pro.local>
2020-05-14 20:45:40 +1000

    Small fixes on creation of new menu item

--- Studio1B/CreateNewMenuItem.swift
+++ Studio1B/CreateNewMenuItem.swift
@@ -15,83 +15,100 @@ class CreateNewMenuItem: UIViewController, UIPickerViewDelegate,
     @IBOutlet weak var description_tf: UITextField!
     @IBOutlet weak var lunchPrice_tf: UITextField!
     @IBOutlet weak var dinnerPrice_tf: UITextField!
-    @IBOutlet weak var menuType: UITextField!
     @IBOutlet weak var dropDown: UIPickerView!
     
     @IBOutlet weak var ErrorMessage: UILabel!
     let db = Firestore.firestore()
     
     var list = ["Dessert","Drinks","Dinner"]
-
-    func errorChecking() -> Bool {
-        return true
+    var menuType = ""
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "backFromNewMenuItem", sender: self)
     }
     
-        func createNewItem() {
-            db.collection("Menu").document(name_tf.text!).setData([
-                "Description": description_tf.text!,
-                "Dinner Price": NSString(string: dinnerPrice_tf.text!).doubleValue,
-                "Lunch Price": NSString(string: lunchPrice_tf.text!).doubleValue,
-                "Type": menuType.text!
-            ]) {err in
-                if let err = err {
-                    print(err)
-                }
-                else{
-                    print("Document added")
-                }
+    func errorChecking() -> Bool {
+        var result = true;
+        let errorColour = UIColor.red
+        
+        // Resetting the borders
+        name_tf.layer.borderWidth = 0
+        dinnerPrice_tf.layer.borderWidth = 0
+        lunchPrice_tf.layer.borderWidth = 0
+        description_tf.layer.borderWidth = 0
+        
+        if (name_tf.text!.isEmpty){
+            name_tf.layer.borderWidth = 1.0
+            name_tf.layer.borderColor = errorColour.cgColor
+            result = false
+        }
+        if (dinnerPrice_tf.text!.isEmpty) || ( NSString(string: dinnerPrice_tf.text!).doubleValue <= 0.0)  {
+            dinnerPrice_tf.layer.borderWidth = 1.0
+            dinnerPrice_tf.layer.borderColor = errorColour.cgColor
+            result = false
+        }
+        if (lunchPrice_tf.text!.isEmpty) || (NSString(string: lunchPrice_tf.text!).doubleValue <= 0.0)  {
+            lunchPrice_tf.layer.borderWidth = 1.0
+            lunchPrice_tf.layer.borderColor = errorColour.cgColor
+            result = false
+        }
+        if (description_tf.text!.isEmpty) {
+            description_tf.layer.borderWidth = 1.0
+            description_tf.layer.borderColor = errorColour.cgColor
+            result = false
+        }
+        return result
+    }
+    
+    func createNewItem() {
+        db.collection("Menu").document(name_tf.text!).setData([
+            "Description": description_tf.text!,
+            "Dinner Price": NSString(string: dinnerPrice_tf.text!).doubleValue,
+            "Lunch Price": NSString(string: lunchPrice_tf.text!).doubleValue,
+            "Type": menuType
+        ]) {err in
+            if let err = err {
+                print(err)
+            }
+            else{
+                print("Document added")
+                self.performSegue(withIdentifier: "menuItemAdded", sender: self)
             }
         }
+    }
     
     @IBAction func create_btn(_ sender: Any) {
         if (errorChecking()){
             createNewItem()
         }
         else{
-            //errorMessage.isHidden = false
+            ErrorMessage.isHidden = false
         }
     }
     
-        public func numberOfComponents(in pickerView: UIPickerView) -> Int{
-            return 1
-        }
-    
-        public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-    
-            return list.count
-        }
-    
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    
-            self.view.endEditing(true)
-            return list[row]
-        }
-    
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-    
-            self.menuType.text = self.list[row]
-            self.dropDown.isHidden = true
-        }
-    
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-    
-            if textField == self.menuType {
-                self.dropDown.isHidden = false
-                //if you don't want the users to se the keyboard type:
-    
-                textField.endEditing(true)
-            }
-        }
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+
+        return list.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.menuType = self.list[row]
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        ErrorMessage.isHidden = true
         self.dropDown.delegate = self
         self.dropDown.dataSource = self
-                // Do any additional setup after loading the view.
-        
-        
     }
 
 }
