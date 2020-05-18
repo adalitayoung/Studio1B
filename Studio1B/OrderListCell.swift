6bb2aa5b1cb03aae7c76237b3b4473aca0514546
Gabrielle Walker <13196732@student.uts.edu.au>
2020-05-18 19:31:58 +1000

    orderlist table created

---
+++ Studio1B/OrderListCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderListCell.swift
+//  Studio1B
+//
+//  Created by Gabrielle Walker on 18/5/20.
+//  Copyright © 2020 davidBolis. All rights reserved.
+//
+
+import UIKit
+
+class OrderListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
